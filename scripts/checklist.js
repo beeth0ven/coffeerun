68314abd2de515cd63bf2a173d8ee15edbe138cf
beeth0ven <beeth0vendev@163.com>
2017-06-17 16:59:48 +0800

    finish from data to dom

---
+++ scripts/checklist.js
@@ -0,0 +1,76 @@
+/**
+ * Created by Air on 2017/6/17.
+ */
+
+(function (window) {
+  "use strict";
+  console.log('checklist.js');
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function CheckList(selector) {
+
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+    this.$element = $(selector);
+    if (this.$element.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+  }
+
+  function Row(coffeeOder) {
+
+    var $div = $('<div></div>', {
+      'data-coffee-order': 'checkbox',
+      'class': 'checkbox'
+    });
+
+    var $label = $('<label></label>');
+
+    var $checkbox = $('<input></input>', {
+      type: 'checkbox',
+      value: coffeeOder.emailAddress
+    });
+
+    var description = coffeeOder.size + ' ';
+    if (coffeeOder.flavor) {
+      description += coffeeOder.flavor + ' ';
+    }
+
+    description += coffeeOder.coffee + ', ';
+    description += ' (' + coffeeOder.emailAddress + ')';
+    description += ' [' + coffeeOder.strength + 'x]';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
+  }
+
+  CheckList.prototype.addClickHandler = function (fn) {
+    this.$element.on('click', 'input', function (event) {
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
+
+  CheckList.prototype.addRow = function (coffeeOder) {
+    this.removeRow(coffeeOder.emailAddress);
+    var rowElement = new Row(coffeeOder);
+    this.$element.append(rowElement.$element);
+  };
+
+  CheckList.prototype.removeRow = function (email) {
+    this.$element
+      .find('[value="' + email + '"]')
+      .closest('[data-coffee-order="checkbox"]')
+      .remove();
+  };
+
+  App.CheckList = CheckList;
+  window.App = App;
+
+})(window);
--- scripts/main.js
+++ scripts/main.js
@@ -6,14 +6,21 @@
   "use strict";
   console.log('main.js');
   var formSelector = '[data-coffee-order="form"]';
+  var checklistSelector = '[data-coffee-order="checklist"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
   var myTruck = new Truck('ncc-1701', new DataStore());
   window.myTruck = myTruck;
+  var checkList = new CheckList(checklistSelector);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(formSelector);
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  formHandler.addSubmitHandler(function (order) {
+    myTruck.createOrder(order);
+    checkList.addRow(order);
+  });
   console.log(formHandler);
 
 })(window);
